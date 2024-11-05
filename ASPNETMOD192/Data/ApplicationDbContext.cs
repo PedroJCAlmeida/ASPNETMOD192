788b6b4849b02c8bfad31a693cfb8f2fb829e6a0
PedroJCAlmeida <pedro.almeida@tecian.pt>
2024-11-05 21:21:33 +0000

    Add project files.

---
+++ ASPNETMOD192/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ASPNETMOD192.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMOD192.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; } = default!; 
+        
+    }
+}
---
+++ ASPNETMOD192/Models/Client.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETMOD192.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateOnly Birthday { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string Address{ get; set; }
+
+        [Required]
+        public string VATNumber { get; set; }
+
+        public DateOnly AdmissionDate { get; set; }
+
+        public DateOnly DeactivationDate{ get; set; }
+
+        public string? CellPhoneNumber { get; set; }
+
+        [EmailAddress] //TODO Check me
+        [Required]
+        public string Email{ get; set; }
+
+
+        //CreatedBy TODO FIXME
+
+    }
+}
