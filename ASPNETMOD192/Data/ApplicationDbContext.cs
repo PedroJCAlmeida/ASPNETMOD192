142eed3fc5a71e1e3942e00b3366ba9df7078e86
PedroJCAlmeida <pedro.almeida@tecian.pt>
2024-11-06 00:19:37 +0000

    Added Staff

--- ASPNETMOD192/Data/ApplicationDbContext.cs
+++ ASPNETMOD192/Data/ApplicationDbContext.cs
@@ -11,7 +11,9 @@ namespace ASPNETMOD192.Data
         {
         }
 
-        public DbSet<Client> Clients { get; set; } = default!; 
-        
+        public DbSet<Client> Clients { get; set; } = default!;
+
+        public DbSet<Staff> Staffs { get; set; } = default!;
+
     }
 }
---
+++ ASPNETMOD192/Models/Staff.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETMOD192.Models
+{
+    public class Staff
+    {
+
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateOnly Birthday { get; set; }
+
+        [Required]
+        public string StaffNumber { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string VATNumber { get; set; }
+
+        public DateOnly AdmissionDate { get; set; }
+
+        public DateOnly DeactivationDate { get; set; }
+
+        public string? CellPhoneNumber { get; set; }
+
+        [EmailAddress] //TODO Check me
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
